17fdbf83db7217835eb5eb32f9bf99265fb66893
Timothy Lam <timothylam1228@gmail.com>
2020-11-09 02:59:27 +0800

    Update start.py

--- start.py
+++ start.py
@@ -316,8 +316,8 @@ def exam(update,context):
     id = context.args[0]
     for row in csv.reader(file):
         if row[1] == id and found == 1:
-            text = text + 'Group' + str(row[3])
-        if row[1] == id:
+            text = text + 'Group' + str(row[3]+'既考試時間係 '+row[5])
+        if row[1] == id and found == 0:
             text= text + str(row[2]+'\nGroup'+row[3]+'\n既考試喺係\n'+row[4]+'既'+row[5])
             found = 1
     context.bot.sendMessage(chat_id=chat_id,text =text)
