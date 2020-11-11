2d634fc5257fe6adb22fd05c2c4b8d9a1ae258fe
Timothy Lam <timothylam1228@gmail.com>
2020-11-12 00:57:01 +0800

    Update start.py

--- start.py
+++ start.py
@@ -364,7 +364,7 @@ def blurPhoto(update, context):
     #img_data = object.get().get('Body').read()
     #print(send_text)
     context.bot.sendMessage(chat_id=chat_id,text =text) 
-    #context.bot.sendPhoto(chat_id=chat_id,photo ='tmp/result.jpg') 
+    context.bot.sendPhoto(chat_id=chat_id,photo =file) 
 
 
 def username(update, context):
