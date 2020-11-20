2e08c1291b52c03da49f45dcf41b6122e9126f88
Timothy Lam <timothylam1228@gmail.com>
2020-11-21 01:48:28 +0800

    Update start.py

--- start.py
+++ start.py
@@ -228,7 +228,7 @@ def show(update,context):
     else:
         text2 = 'You 比人屌左'+ str(target)+'次'
         #@update.message.reply_text(text = 'You 比人屌左'+ str(target)+'次')
-    update.message.reply_text(text = text1 +'\n\n'+text2)
+    update.message.reply_text(text "Broked no want fix ")
     
 
 def listCanteen(update,context):
