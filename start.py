2d096543a8f1c35d08ac8d0edcdf5341a69d55be
Timothy Lam <timothylam1228@gmail.com>
2021-03-25 00:11:33 +0800

    Update start.py

--- start.py
+++ start.py
@@ -322,7 +322,8 @@ def week(update,context):
     chat_id=update.message.chat.id
     today = datetime.today()
     week_number = today.isocalendar()[1]
-    update.message.reply_text(text='INFO'+str(week_number))
+    week_number = week_number-4
+    update.message.reply_text(text='Now is Week '+str(week_number))
 
 def exam(update,context):
     found = 0
