c722498dc4416dc63fe58185ba62a22a04fffe50
EvaChan <72225181+evachan246@users.noreply.github.com>
2021-04-10 00:34:53 +0800

    Update start.py

--- start.py
+++ start.py
@@ -399,7 +399,7 @@ def main():
 
     updater.start_webhook(listen="0.0.0.0",
                           port=8443,
-                          port=int(PORT),
+#                          port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://serene-depths-59599.herokuapp.com/' + TOKEN)
 
