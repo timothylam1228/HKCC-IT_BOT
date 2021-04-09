4e0d12cd48bff0c02b289a73520e4fa3c1e58535
evachan246 <chingwa0812@gmail.com>
2021-04-09 23:59:44 +0800

    Update start.py

--- start.py
+++ start.py
@@ -407,7 +407,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('serene-depths-59599.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://serene-depths-59599.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
