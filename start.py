173304a89173c2a5ecd1dec73d4824f8d099526c
evachan246 <chingwa0812@gmail.com>
2021-06-13 01:25:09 +0800

    Update start.py

--- start.py
+++ start.py
@@ -394,31 +394,40 @@ def checkTemp(update, context):
 
 def samgor(update, context):
     chat_id=update.message.chat.id
-    f = open('samgor.json',)
-    data = json.load(f)
-    randomIngredient = random.sample(range(25), 3)
-    randomSoupBase = random.sample(range(6), 1)
-    randomSpiciness = random.sample(range(10), 1)
-    randomAppetizers = random.sample(range(9), 1)
-    randomSnacksForOne = random.sample(range(6), 1)
-    randomDrink = random.sample(range(15), 1)
-    eattext=''
-    eattext='今日三哥食咩好\n'
-    eattext = eattext + '三餸 : \n'
-    eattext = eattext + data['Ingredient'][randomIngredient[0]]['name'] + ' '+data['Ingredient'][randomIngredient[1]]['name'] + ' '+data['Ingredient'][randomIngredient[2]]['name'] + '\n'
-    eattext = eattext + '湯底 : \n'
-    eattext = eattext + data['Spiciness'][randomSoupBase[0]]['name'] + '\n'
-    eattext = eattext + '辣度 : \n'
-    if randomSoupBase == 4 or randomSoupBase == 5 :
-        eattext = eattext + '無辣比你揀呀 88 \n'
-    else :
-        eattext = eattext + data['Appetizers'][randomSpiciness[0]]['name'] + '\n'
-    eattext = eattext + '三哥小食 : \n'
-    eattext = eattext + data['SnacksForOne'][randomAppetizers[0]]['name'] + '\n'
-    eattext = eattext + '飲品 : \n'
-    eattext = eattext + data['Drink'][randomDrink[0]]['name'] + '\n'
-    context.bot.sendMessage(chat_id=chat_id,text =eattext)
-
+    try:
+        number1 = int(context.args[0])
+        if(number1>26 or number1<0):
+            update.message.reply_text('請輸入範圍 1-25')
+            return
+        f = open('samgor.json',)
+        data = json.load(f)
+        randomIngredient = random.sample(range(25), number1)
+        randomSoupBase = random.sample(range(6), 1)
+        randomSpiciness = random.sample(range(10), 1)
+        randomAppetizers = random.sample(range(9), 1)
+        randomSnacksForOne = random.sample(range(6), 1)
+        randomDrink = random.sample(range(15), 1)
+        eattext=''
+        eattext='今日三哥食咩好\n'
+        eattext = eattext + number1 + '餸 : \n'
+        for i in number1 :
+            eattext = eattext + data['Ingredient'][randomIngredient[i]]['name']# + ' '+data['Ingredient'][randomIngredient[1]]['name'] + ' '+data['Ingredient'][randomIngredient[2]]['name'] + '\n\n'
+        eattext = eattext + '湯底 : \n'
+        eattext = eattext + data['SoupBase'][randomSoupBase[0]]['name'] + '\n\n'
+        eattext = eattext + '辣度 : \n'
+        if randomSoupBase == 4 or randomSoupBase == 5 :
+            eattext = eattext + '無辣比你揀呀 88 \n'
+        else :
+            eattext = eattext + data['Spiciness'][randomSpiciness[0]]['name'] + '\n\n'
+        eattext = eattext + '三哥小食 : \n'
+        eattext = eattext + data['Appetizers'][randomAppetizers[0]]['name'] + '\n\n'
+        eattext = eattext + '一人小食 : \n'
+        eattext = eattext + data['Spiciness'][randomSnacksForOne[0]]['name'] + '\n\n'
+        eattext = eattext + '飲品 : \n'
+        eattext = eattext + data['Drink'][randomDrink[0]]['name'] + '\n'
+        context.bot.sendMessage(chat_id=chat_id,text =eattext)
+    except (IndexError, ValueError):
+        update.message.reply_text('There are not enough numbers')
 
 def payment(update, context):
     chat_id=update.message.chat.id
