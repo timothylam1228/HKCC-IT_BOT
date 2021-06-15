e0ef4f949731c4b98c943d38320f6cc91cf72c0b
Timothy Lam <timothylam1228@gmail.com>
2021-06-16 00:07:45 +0800

    asd

--- start.py
+++ start.py
@@ -316,7 +316,7 @@ def pin9(update,context):
     f = open("pin.txt", "r")
     temp = f.read()
     # update.message.reply_text(text='INFO'+temp)
-    context.bot.sendMessage(chat_id=chat_id,text=temp, parse_mode= 'Markdown')
+    context.bot.sendMessage(chat_id=chat_id,text = temp, parse_mode= 'HTML')
 
 
 
