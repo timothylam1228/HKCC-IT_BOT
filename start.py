8451be760903228375f539ed9395735e17e1557c
timothylam1228 <38665439+timothylam1228@users.noreply.github.com>
2022-03-15 15:53:32 +0800

    set webhook

--- start.py
+++ start.py
@@ -179,9 +179,10 @@ def main():
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook(
-        'https:/hkcc-it-bot.herokuapp.com/' + TOKEN)
+                          url_path=TOKEN,
+                          webhook_url='https:/hkcc-it-bot.herokuapp.com/' + TOKEN)
+    # updater.bot.setWebhook(
+    #     'https:/hkcc-it-bot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
