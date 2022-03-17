29e91d1911a2a69dc2ff0804e4c91f7da9bb2507
timothylam1228 <38665439+timothylam1228@users.noreply.github.com>
2022-03-17 09:09:16 +0800

    Bam function

--- start.py
+++ start.py
@@ -42,11 +42,10 @@ def main():
 
 
     #Bot
-    dp.add_handler(MessageHandler(
-        Filters.status_update.new_chat_members, newmember))
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, newmember))
+    dp.add_handler(CommandHandler('bam',ban, filters=Filters.chat_type.groups))
     dp.add_handler(CommandHandler("pin9", pin9, filters=Filters.chat_type.groups))
     dp.add_handler(CommandHandler("username", username, pass_args=True))
-
     dp.add_handler(CommandHandler("dllmcount", dllmcount, filters=~Filters.chat_type.groups))
     dp.add_handler(CommandHandler("show_dllm",show_dllm))
 
