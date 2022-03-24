e694580471983c30c675c83dbafb214501f206af
timothylam1228 <38665439+timothylam1228@users.noreply.github.com>
2022-03-24 19:49:45 +0800

    update pin9

--- commands/bot/pin9.py
+++ commands/bot/pin9.py
@@ -1,6 +1,6 @@
 def pin9(update, context):
     chat_id = update.message.chat.id
-    f = open("pin.txt", "r")
+    f = open("public/pin.txt",encoding="utf8")
     temp = f.read()
     # update.message.reply_text(text='INFO'+temp)
     context.bot.sendMessage(chat_id=chat_id, text=temp, parse_mode='HTML')
